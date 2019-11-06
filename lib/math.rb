70a1275b259e6a4b6c659441c7ac863067becc89
Tyree Williams <tyreewilliams515@gmail.com>
2019-11-06 03:49:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,27 @@
 def addition(num1, num2)
-
+result = num1 + num2
+return result
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  return result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  return result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
-
+  return result = (num1 / num2).Integer
 end
 
 def square_root(num)
-
+return Math.sqrt(num)
 end
